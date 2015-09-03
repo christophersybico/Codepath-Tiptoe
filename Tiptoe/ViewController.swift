1438d127fb24aab3d50d8430040f9eea6acf99da
christophersybico <christophersybico@gmail.com>
2015-09-03 02:03:08 +0200

    By tapping the tip row, it should change the bg.

--- Tiptoe/ViewController.swift
+++ Tiptoe/ViewController.swift
@@ -14,29 +14,51 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var tipLabel: UILabel!
     
+    let tipLabelTapRecognizer = UITapGestureRecognizer()
+    
     @IBOutlet weak var totalLabel: UILabel!
     
     @IBOutlet weak var tipControl: UISegmentedControl!
     
+    @IBOutlet weak var appBgBillView: UIImageView!
+    
+    @IBOutlet weak var appBgTipView: UIImageView!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        // Set Tip and Total to $0.00
         tipLabel.text = "0.00"
         totalLabel.text = "0.00"
         
         // Focus on bill textfield
         billField.becomeFirstResponder()
+        
+        // Set the target of the tap recognizer
+        tipLabelTapRecognizer.addTarget(self, action: "showTipPicker")
+
+        // Add recognizer to tipLabel
+        tipLabel.addGestureRecognizer(tipLabelTapRecognizer)
+        
+        // Add recognizer to tipLabel
+        tipLabel.userInteractionEnabled = true
+        
+        appBgBillView.alpha = 1
+        appBgTipView.alpha = 0
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+       
+        // Dispose of any resources that can be recreated.
     }
     
     // Hide Status Bar
     override func prefersStatusBarHidden() -> Bool {
         return true
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     @IBAction func onEditingChanged(sender: AnyObject) {
@@ -58,8 +80,17 @@ class ViewController: UIViewController {
         
     }
     
-    @IBAction func onTap(sender: AnyObject) {
-        view.endEditing(true)
+    @IBAction func tapOnBillField(sender: AnyObject) {
+        appBgBillView.alpha = 1
+        appBgTipView.alpha = 0
     }
+    
+    func showTipPicker(){
+        print("picker")
+        view.endEditing(true)
+        appBgBillView.alpha = 0
+        appBgTipView.alpha = 1
+    }
+    
 }
 
